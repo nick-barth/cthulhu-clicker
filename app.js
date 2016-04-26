3ea5084b449a64497c00a7d2cd80b87db7014c6f
Nick Barth <barth.nicholas@gmail.com>
2016-04-25 23:03:16 -0400

    whew, its getting started

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+(function(angular) {
+    'use strict';
+    var app = angular.module('InsanityApp', []);
 
+    app.controller('LunacyCtrl', ['$scope', '$interval', function($scope, $interval) {
+        $scope.madness = 0;
+        $scope.madness_rate = 0;
+        $scope.click_rate = 1;
+        $scope.total_madness = 0;
 
+        $scope.madness_upgrades = [
+          {
+            id: 0,
+            name: 'Hooded Chanter',
+            desc: "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn",
+            total: 0,
+            cost: 10,
+            cost_increase: function(){
+              return this.total * 2;
+            },
+            increase: 1,
+            modifier: 0,
+            visible: 1
+          }
+        ];
 
-$( document ).ready(function() {
-  var madness = 0;
-  var madness_rate = 1;
+        $interval(function() {
+            $scope.madness = $scope.madness + $scope.madness_rate;
+            $scope.total_madness = $scope.total_madness + $scope.madness_rate;
+        }, 1000);
 
-  window.setInterval(function(){
-    madness = madness + madness_rate;
-  }, 1000);
+        $scope.click = function(){
+          $scope.madness = $scope.madness + $scope.click_rate;
+          $scope.total_madness = $scope.total_madness + $scope.madness_rate;
+        }
+        $scope.upgradeClick = function(id){
+          var upgrade = $scope.madness_upgrades[id];
+          $scope.madness = $scope.madness - upgrade.cost;
+          $scope.madness_rate = $scope.madness_rate + upgrade.increase;
+          upgrade.total++;
+          upgrade.cost = upgrade.cost + upgrade.cost_increase();
+        }
 
-});
+    }]);
+})(window.angular);
